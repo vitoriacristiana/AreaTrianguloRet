d47252e9d9704d68acdc883c23ae9a089b04c01d
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 13:19:20 -0300

    Exercício concluido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            double b, h, area;
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite o valor da base:");
+            b= double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor da altura:");
+            h= double.Parse(Console.ReadLine());
+            area= b * h / 2;
+            Console.Beep();
+            Console.Write($"Área do triangulo retangulo:");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(area);
+            Console.ResetColor();
+            }
+        }
+    }
+
